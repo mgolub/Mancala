950d77e592d8a770c00f95f632b8852a7e00ff59
student1 <student@touro.edu>
2016-03-31 21:04:55 -0400

    fixed speed error- maintains consistant speed

--- src/main/java/mancala/PieceAnimation.java
+++ src/main/java/mancala/PieceAnimation.java
@@ -7,9 +7,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class PieceAnimation extends JComponent {
+public class PieceAnimation extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private int cupNumber;
@@ -20,47 +21,16 @@ public class PieceAnimation extends JComponent {
 	private Timer timer;
 	private final int GOAL1 = 7;
 	private final int GOAL2 = 14;
+	private Image piece;
 
-	public PieceAnimation(int cupNumber, int pieceAmount) {
+	public PieceAnimation(CupComponent[] cupComponents,int cupNumber, int pieceAmount) {
 
+		piece = new ImageIcon(getClass().getResource("/BlueMarble.png")).getImage();
 		this.cupNumber = cupNumber + 1;// assume cupNumber starts at 0
 		this.pieceAmount = pieceAmount;
-
-		//if cup on top row set place by *100+100
-		if (this.cupNumber > 0 && this.cupNumber < 7) {
-			yAxis = cupNumber * 100 +100;
-			xAxis = 100;
-		} else {
-			//if on bottom row or goal manually set place
-			xAxis = 500;
-			switch (this.cupNumber) {
-			case 13:
-				yAxis = 200;
-				break;
-			case 12:
-				yAxis = 300;
-				break;
-			case 11:
-				yAxis = 400;
-				break;
-			case 10:
-				yAxis = 500;
-				break;
-			case 9:
-				yAxis = 600;
-				break;
-			case 8:
-				yAxis = 700;
-				break;
-			case 7:
-				yAxis = 800;
-				xAxis = 300;
-				break;
-			case 14:
-				yAxis = 100;
-				xAxis = 300;
-			}
-		}
+		
+		xAxis = cupComponents[cupNumber].getX();
+		yAxis = cupComponents[cupNumber].getY();
 		yAxisTemp = yAxis;
 		repaint();
 		ActionListener animater = new ActionListener() {
@@ -81,39 +51,9 @@ public class PieceAnimation extends JComponent {
 		super.paintComponent(g);
 
 		for (int i = 0; i < pieceAmount; i++) {
-			Image piece = new ImageIcon(getClass().getResource("/BlueMarble.png")).getImage();
 			
-			//if in cup 1 go lower left to GOAL2
-			if (cupNumber == 1){
-				g.drawImage(piece, yAxis, (i * 15) + xAxis, this);
-				yAxis--;
-				xAxis++;
-			}
-			//if in GOAL2 go lower right to cup 13
-			else if (cupNumber == GOAL2){
-				g.drawImage(piece, yAxis, (i * 15)+ xAxis, this);
-				yAxis++;
-				xAxis++;
-			}
-			//if in top row go left
-			else if (cupNumber > 0 && cupNumber < GOAL1) {
-				g.drawImage(piece, yAxis, (i * 15) + xAxis, this);
-				yAxis--;
-			} 
-			//if in bottom row go right
-			else if (cupNumber > GOAL1 && cupNumber < GOAL2){
-				g.drawImage(piece, yAxis, (i * 15) + xAxis, this);
-				yAxis++;
-			}
-			//if in GOAL1 up upper left
-			else if (cupNumber == GOAL1){
-				g.drawImage(piece, yAxis, (i * 15)+ xAxis, this);
-				yAxis--;
-				xAxis--;
-			}
+			g.drawImage(piece, yAxis, (i * 15)+ xAxis, this);
 			
-			
-
 			// if marbles reach next cup
 			
 			//if in cup 1 to go to GOAL2
@@ -143,6 +83,30 @@ public class PieceAnimation extends JComponent {
 				timer.stop();
 				g.clearRect(0, 0, getWidth(), getHeight());
 			}
+		}
+		
+		//if in cup 1 go lower left to GOAL2
+		if (cupNumber == 1){
+			yAxis--;
+			xAxis++;
+		}
+		//if in GOAL2 go lower right to cup 13
+		else if (cupNumber == GOAL2){
+			yAxis++;
+			xAxis++;
+		}
+		//if in top row go left
+		else if (cupNumber > 0 && cupNumber < GOAL1) {
+			yAxis--;
+		} 
+		//if in bottom row go right
+		else if (cupNumber > GOAL1 && cupNumber < GOAL2){
+			yAxis++;
+		}
+		//if in GOAL1 up upper left
+		else if (cupNumber == GOAL1){
+			yAxis--;
+			xAxis--;
 		}
 
 	}
--- src/main/java/mancala/TempMainForAnimation.java
+++ src/main/java/mancala/TempMainForAnimation.java
@@ -5,8 +5,33 @@ import javax.swing.JFrame;
 
 public class TempMainForAnimation {
 
+	private static CupComponent[] cupComponents;
 	public static void main(String[] args) {
-		JComponent animation = new PieceAnimation(5, 34);
+		
+		cupComponents = new CupComponent[13];
+			for (int i = 0; i < Board.GOAL1; i++) {
+				cupComponents[i] = new CupComponent(647, 1010);// For testing the
+																// x,y
+			//	cupPanel1.add(cupComponents[i]);
+			//	cupComponents[i].setToolTipText(Integer.toString(board.getContent(i)));
+			}
+			for (int i = 12; i >= 7; i--) {
+				cupComponents[i] = new CupComponent(639, 1560); // For testing the
+																// x,y
+				//cupPanel2.add(cupComponents[i]);
+			//	cupComponents[i].setToolTipText(Integer.toString(board.getContent(i)));
+			}
+
+			//cupComponents[Board.GOAL1] = new GoalComponent(0, 0);
+			//cupComponents[Board.GOAL1].setToolTipText(Integer.toString(board.getContent(Board.GOAL1)));
+			//cupComponents[Board.GOAL2] = new GoalComponent(0, 0);
+			//cupComponents[Board.GOAL2].setToolTipText(Integer.toString(board.getContent(Board.GOAL2)));
+
+	
+
+		
+		
+		JComponent animation = new PieceAnimation(cupComponents,5, 34);
 		JFrame app = new JFrame();
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		app.add(animation);
